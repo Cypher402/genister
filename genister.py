41471571995e3573ea22497d7115d160d6c35e66
Evan Porteous <code@philasky.com>
2021-07-27 20:48:41 -0700

    Refactor.  Moved lines outside of function.  Improved comments.

--- genister.py
+++ genister.py
@@ -1,6 +1,13 @@
 import random, os
 from os import system, name
 
+# Create list "lines"
+# First using "open()" to open the text doc so that we can read from or write to it.
+# Using "read()" to be able to read the doc contents.
+# Splitline() method is used to split the lines at line boundaries.
+# The use of split is very common in most languages and is used by strings to break them up into an array.
+lines = open('genister_store.txt').read().splitlines()
+
 def quit_program():
     clear_terminal()
     print("\n\t**Philasky Bot bids you farewell and a good day!**\n")
@@ -35,29 +42,23 @@ def main_menu():
             quit_program()
 
 def copy_clipboard(text):
-    #pbcopy = OSX - clip = Win - xclip = Linux
-    #he strip() method removes any leading (spaces at the beginning) and trailing (spaces at the end) characters (space is the default leading character to remove)
+    # Echo is used to specify that we are copying the terminal text
+    # pbcopy = OSX - clip = Win - xclip = Linux
+    # The strip() method removes any leading (spaces at the beginning) and trailing (spaces at the end) characters (space is the default leading character to remove)
     command = 'echo ' + text.strip() + '| clip'
     os.system(command)
 
-def random_line(rand_line):
-    #first using "open()" to open the text doc so that we can read from or write to it.
-    #using "read()" to be able to read the doc contents.
-    #splitline() method is used to split the lines at line boundaries. 
-    #The function returns a list of lines in the string.
-    lines = open(rand_line).read().splitlines()
-
+def random_line(lines):
+    # When the function is called, return a random word(line) from "lines" list that is opening the txt doc.
     return random.choice(lines)
 
 def generate_pass():
     clear_terminal()
-    word1 = (random_line('genister_store.txt'))
-    word2 = (random_line('genister_store.txt'))
-    word3 = (random_line('genister_store.txt'))
-
+    word1, word2, word3 = random_line(lines), random_line(lines), random_line(lines)
     copy_clipboard(f"{word1} {word2} {word3}")
-    #Return the length (the number of items) of an object. 
-    #The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
+
+    # Return the length (the number of items) of an object. 
+    # The argument may be a sequence (such as a string, bytes, tuple, list, or range) or a collection (such as a dictionary, set, or frozen set).
     print("\nThe password character length is: ", len(f"{word1} {word2} {word3}"))
     print("\n************************************")
     print(f"   {word1} {word2} {word3}")
